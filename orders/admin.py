e48af43a03482f1452716c7c11cae847ae952d37
IndikaMaligaspe <k.indika.maligaspe@gmail.com>
2020-04-21 16:23:31 +0530

    extended admin to display order details

--- orders/admin.py
+++ orders/admin.py
@@ -3,6 +3,8 @@ import datetime
 from django.http import HttpResponse
 from django.contrib import admin
 from .models import Order, OrderItem
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 
@@ -25,13 +27,18 @@ def export_to_csv(modeladmin, request, queryset):
     return response
 export_to_csv.short_description = 'Export to CSV'
 
+def order_detail(obj):
+    return mark_safe('<a href="{}">View</a>'.format(reverse('orders:admin_order_detail',args=[obj.id])))
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
 
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id','first_name','last_name','email','address','postal_code','city','paid','created','updated']
+    list_display = ['id','first_name','last_name','email','address','postal_code','city','paid','created','updated',order_detail]
     list_filter = ['paid','created','updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
