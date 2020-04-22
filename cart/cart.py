a3e0bc192c7ef57a5d59d4ac10330bfae2e5dc16
IndikaMaligaspe <k.indika.maligaspe@gmail.com>
2020-04-22 13:45:20 +0530

    started adding coupons

--- cart/cart.py
+++ cart/cart.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from shop.models import Product
+from coupons.models import Coupon
 
 class Cart(object):
     def __init__(self, request):
@@ -8,11 +9,20 @@ class Cart(object):
             Initialize the cart
         '''
         self.session = request.session
+        self.coupon_id = self.session.get('coupon_id')
         cart = self.session.get(settings.CART_SESSION_ID)
 
         if not cart:
             cart =  self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            return Coupon.objects.get(id=self.coupon_id)
+        return None
+
 
     def add(self, product, quantity, update_quantity=False):
         product_id = str(product.id)
@@ -28,12 +38,23 @@ class Cart(object):
 
     def save(self):
         self.session.modified = True
-    
+
+
     def remove(self, product):
         product_id = str(product.id) 
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
+
+
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal('100')) * self.get_total_price()
+        return Decimal('0')
+
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
 
 
     def __iter__(self):
---
+++ coupons/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+# Create your models here.
+
+class Coupon(models.Model):
+    code = models.CharField( max_length=50, unique=True)
+    valid_from = models.DateTimeField(auto_now=False, auto_now_add=False)
+    valid_to = models.DateTimeField(auto_now=False, auto_now_add=False)
+    discount = models.IntegerField(validators = [MinValueValidator(0), MaxValueValidator(100)])
+    active = models.BooleanField()
+
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse("Coupons_detail", kwargs={"pk": self.pk})
+
